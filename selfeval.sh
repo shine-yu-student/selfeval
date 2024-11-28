34e28d8c774d05b62bbfd13f3696778e100243bc
Shine Yu <105355837+shine-yu-student@users.noreply.github.com>
2024-11-28 13:19:49 +0800

    Update selfeval.sh

--- selfeval.sh
+++ selfeval.sh
@@ -5,6 +5,12 @@ tfm=$TIMEFORMAT
 TIMEFORMAT='%3R'
 debug_patch=0
 
+if (($# < 1))
+then
+	echo "Error: missing workspace path"
+	exit 1
+fi
+
 if (($# > 1))
 then
 	if (($2 == "-d"))
@@ -18,13 +24,15 @@ readarray -t arr -d "\n" < $config_file
 workspace_dir="${arr[0]}"
 sandbox_dir="${arr[1]}"
 checker_dir="${arr[2]}"
-compile_args="${arr[3]}"
+compile_args_std="${arr[3]}"
+compile_args_checker="${arr[4]}"
+previous_checker="dummy"
 
 mkdir $sandbox_dir
 
 array_size="${#arr[@]}"
 
-ptr=4
+ptr=5
 
 while (($ptr<array_size))
 do
@@ -36,20 +44,28 @@ do
 	time_limit=$(echo "${subarr[4]}*0.001" | bc)"s"
 	echo "Judging task $task_name:"
 	printf "Compiling checker: "
-	g++ $checker_dir/$checker_name.cpp -o $sandbox_dir/checker -std=c++20 -O2 --static
-	result=$?
-	if ((result > 0))
+	if [ "$previous_checker" != "$checker_name" ]
 	then
-		printf "failed returning %d.\n" $result
-		continue
+		g++ $checker_dir/$checker_name.cpp -o $sandbox_dir/checker $compile_args_checker
+		result=$?
+		if ((result > 0))
+		then
+			printf "failed returning %d.\n" $result
+			ptr=$(expr $ptr + 1)
+			continue
+		fi
+		printf "done.\n"
+		previous_checker=$checker_name
+	else
+		printf "skipped.\n"
 	fi
-	printf "done.\n"
 	printf "Compiling std: "
-	g++ $workspace_dir/$task_name/$task_name.cpp -o $sandbox_dir/std $compile_args
+	g++ $workspace_dir/$task_name/$task_name.cpp -o $sandbox_dir/std $compile_args_std
 	result=$?
 	if ((result > 0))
 	then
 		printf "failed returning %d.\n" $result
+		ptr=$(expr $ptr + 1)
 		continue
 	fi
 	printf "done.\n"
@@ -57,13 +73,18 @@ do
 	tptr=1
 	while [ $tptr -le $task_number ]
 	do
-		printf "Running on task $tptr:\n"
+		if ((debug_patch == 0))
+		then
+			printf "\nRunning on task $tptr:\n"
+		else
+			printf "Running on task $tptr:\n"
+		fi
 		file_name=${file_pattern/\{\}/$tptr}
 		cp $workspace_dir/$task_name/$file_name.in $task_name.in
 		cp $workspace_dir/$task_name/$file_name.ans $task_name.ans
-		proc_time=$((time timeout $time_limit ./std) 2>&1)
+		proc_time=$((time /usr/bin/timeout $time_limit ./std) 2>&1)
 		result=$?
-		if((result == 124))
+		if ((result == 124))
 		then
 			printf "Time limit exceeded.\n"
 			tptr=$(expr $tptr + 1)
@@ -77,8 +98,8 @@ do
 			read
 		fi
 	done
-	rm ./$task_name.in ./$task_name.out ./$task_name.ans ./std ./checker
+	rm ./$task_name.in ./$task_name.out ./$task_name.ans ./std
 	ptr=$(expr $ptr + 1)
 done
+rm ./checker
 rmdir $sandbox_dir
-# /home/tyoi/yse/oiclass/67431ae9752b8de081989ccf /home/tyoi/yse/oiclass/67431ae9752b8de081989ccf/sandbox
